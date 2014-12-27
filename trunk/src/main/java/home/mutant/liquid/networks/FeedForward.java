856140040409e38b074c482a022328b71a625a8c
mutantu2002@gmail.com <mutantu2002@gmail.com@c79dd4fb-b08f-bbd9-498d-e0bf887acddc>
2014-12-27 19:02:20 +0000

    Show network

--- trunk/src/main/java/home/mutant/liquid/networks/FeedForward.java
+++ trunk/src/main/java/home/mutant/liquid/networks/FeedForward.java
@@ -3,11 +3,13 @@ package home.mutant.liquid.networks;
 import home.mutant.liquid.cells.NeuronCell;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FeedForward
 {
-	List<List<NeuronCell>> layers = new ArrayList<List<NeuronCell>>();
+	public final static int NO_SYNAPSES=6;
+	public List<List<NeuronCell>> layers = new ArrayList<List<NeuronCell>>();
 	
 	public FeedForward(int noLayers, int NoNeuronsPerLayer) 
 	{
@@ -16,9 +18,27 @@ public class FeedForward
 			List<NeuronCell> layer = new ArrayList<NeuronCell>();
 			for (int j=0;j<NoNeuronsPerLayer;j++)
 			{
-				layer.add(new NeuronCell(4));
+				layer.add(new NeuronCell(NO_SYNAPSES));
 			}
 			layers.add(layer);
+		}
+	}
+	public void step(double[] input)
+	{
+		for (int noLayer=layers.size()-1;noLayer>0;noLayer--) 
+		{
+			List<NeuronCell> layer = layers.get(noLayer);
+			List<NeuronCell> layerBelow = layers.get(noLayer-1);
+			for (int noNeuron=0;noNeuron<layerBelow.size()-NO_SYNAPSES+1;noNeuron++)
+			{
+				layer.get(noNeuron).outputProbability(layerBelow.subList(noNeuron, noNeuron+NO_SYNAPSES));
+			}
+		}
+		
+		List<NeuronCell> layer0 = layers.get(0);
+		for (int noNeuron=0;noNeuron<input.length-NO_SYNAPSES+1;noNeuron++)
+		{
+			layer0.get(noNeuron).outputProbability(Arrays.copyOfRange(input, noNeuron, noNeuron+NO_SYNAPSES));
 		}
 	}
 }
