7324e5c191965c6943d44fd88a1306056d16f58b
mutantu2002@gmail.com <mutantu2002@gmail.com@c79dd4fb-b08f-bbd9-498d-e0bf887acddc>
2015-01-19 20:19:25 +0000

    Multi thread sub images

---
+++ src/main/java/home/mutant/liquid/mains/OutputNeuronsRunnable.java
@@ -0,0 +1,33 @@
+package home.mutant.liquid.mains;
+
+import home.mutant.liquid.cells.NeuronCell;
+
+import java.util.List;
+
+public class OutputNeuronsRunnable implements Runnable
+{
+	public OutputNeuronsRunnable(List<NeuronCell> net) 
+	{
+		super();
+		this.net = net;
+	}
+
+	public List<byte[]> subImages;
+	List<NeuronCell> net;
+	
+	@Override
+	public void run() 
+	{
+		for (byte[] subImage : subImages) 
+		{
+			for (NeuronCell neuron : net)
+			{
+				if (neuron.isFiring(subImage))
+				{
+					neuron.modifyWeights(subImage);
+				}
+			}
+		}
+	}
+
+}
